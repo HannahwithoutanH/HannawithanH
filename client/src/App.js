dec75ab483843d52469e7c8d3a2c823661953b71
Tsion Adefres <tadefres@gmail.com>
2018-10-08 14:13:56 -0700

    React app boilerplate with an Express backend

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          HannaH without an H
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  res.json([{ id: 1, username: 'teamHannah' }]);
+});
+
+module.exports = router;
