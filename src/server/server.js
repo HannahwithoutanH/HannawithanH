9523f2a25579448537f453a48aa91ecbeb32bb26
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-10-09 11:42:21 -0700

    connected to postgres db

--- src/server/server.js
+++ src/server/server.js
@@ -10,9 +10,18 @@ const app = express();
 // re-usable open client instances (reduces latency whenever a client can be reused). You definitely do 
 // not want to call pool.end() when your query completes, you want to reserve that for when your application 
 // terminates because pool.end() disposes of all the open client instances.
-const pool = new Pool(process.env.PGDATABASE);
+const pool = new Pool(process.env.POSTGRES_URI);
 
-pool.connect();
+pool.connect(err => {
+  if (err) return console.error('Could not connect to postgres', err);
+  else {
+    pool.query('SELECT NOW() AS "theTime"', (err, result) => {
+      if (err) return console.error('error running query', err);
+      console.log(result.rows[0].theTime);
+      // client.end();
+    });
+  }
+});
 
 app.listen(3000, (err) => {
   if (err) console.log(err);
