6aad7361ed0fcc1eda97ca17e8a150cd4d161938
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-10-09 10:27:56 -0700

    pre merge

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const { Pool } = require('pg')
 const app = express();
+
+// pools will use environment variables
+// for connection information
+
+// Use a pool if you have or expect to have multiple concurrent requests. It is there to provide a pool of 
+// re-usable open client instances (reduces latency whenever a client can be reused). You definitely do 
+// not want to call pool.end() when your query completes, you want to reserve that for when your application 
+// terminates because pool.end() disposes of all the open client instances.
+const pool = new Pool();
+
+pool.connect();
 
 app.listen(3000, (err) => {
   if (err) console.log(err);
