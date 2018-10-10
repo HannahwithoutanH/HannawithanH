f9eedaf4a5f423bfbbff6b800278c95ad0037b66
Tsion Adefres <tadefres@gmail.com>
2018-10-09 19:59:06 -0700

    Next.js starter files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,28 @@
-import Router from 'next/router'
+import Router from 'next/router';
 
 export default () => (
   <div>
-    <Link href='/'>Home</Link>
-    <Link href='/about'>About</Link>
-    <Link href='/error'>Error</Link>
+    <Link href="/">Home</Link>
   </div>
-)
+);
 
-// typically you want to use `next/link` for this usecase
-// but this example shows how you can also access the router
-// and use it manually
-
-function onClickHandler (href) {
+function onClickHandler(href) {
   return (e) => {
-    e.preventDefault()
-    Router.push(href)
-  }
+    e.preventDefault();
+    Router.push(href);
+  };
 }
 
 const Link = ({ children, href }) => (
-  <a href='#' onClick={onClickHandler(href)}>
+  <a href="#" onClick={onClickHandler(href)}>
     {children}
-    <style jsx>{`
+    <style jsx>
+      {`
       a {
         margin-right: 10px;
       }
-    `}</style>
+    `}
+
+    </style>
   </a>
-)
+);
