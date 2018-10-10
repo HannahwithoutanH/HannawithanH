f5ce9fa0924ceb2e37207ca00bdd831c3ceda3e3
seano <sth.xxx@gmail.com>
2018-10-09 19:43:19 -0700

    adding tests, jest

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const app = express();
+const server = express();
 
-app.listen(3000, (err) => {
+
+
+
+sever.listen(3000, (err) => {
   if (err) console.log(err);
   else console.log('Listening on port 3000...');
 })
+
+
+module.exports = server;
