4573ab1111cd4a945299ef45129f31accf4d0673
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-10-09 17:48:03 -0700

    database setup

--- src/server/server.js
+++ src/server/server.js
@@ -1,27 +1,18 @@
 const express = require('express');
 require('dotenv').config();
-const { Pool } = require('pg')
 const app = express();
+const db = require('./bd.js');
 
-// pools will use environment variables
-// for connection information
+// const practiceUser = ['GD', 'faker@gmail.com', '91320', 'general', 'metaco123', '5555555'];
+// const practiceEvent = ['1', 'Cuts', 'hair cuts', 'here', '2008-11-11 13:23:44', '0', 'a place to cut hair', 'pending'];
 
-// Use a pool if you have or expect to have multiple concurrent requests. It is there to provide a pool of 
-// re-usable open client instances (reduces latency whenever a client can be reused). You definitely do 
-// not want to call pool.end() when your query completes, you want to reserve that for when your application 
-// terminates because pool.end() disposes of all the open client instances.
-const pool = new Pool(process.env.POSTGRES_URI);
+// db.insertUser(...practiceUser);
+// db.insertEvent(...practiceEvent);
+// db.insertRSVPS(4, 25);
 
-pool.connect(err => {
-  if (err) return console.error('Could not connect to postgres', err);
-  else {
-    pool.query('SELECT NOW() AS "theTime"', (err, result) => {
-      if (err) return console.error('error running query', err);
-      console.log(result.rows[0].theTime);
-      // client.end();
-    });
-  }
-});
+db.getUser('fake@gmail.com');
+// db.getEventByService('');
+// db.getEventByLocation('');
 
 app.listen(3000, (err) => {
   if (err) console.log(err);
