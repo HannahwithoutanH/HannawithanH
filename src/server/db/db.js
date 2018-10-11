fd697aabf0332896af035afcc397bda76ceb4e19
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-10-10 17:01:07 -0700

    changing database

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -3,74 +3,120 @@ const pgp = require('pg-promise')();
 const db = pgp(process.env.ELEPHANT_SQL);
 
 module.exports = {
-  insertUser: async (...args) => {
+  // =====>Users<===== \\
+  addUser: async (...args) => {
     // six parameters must be passed
-    if (args.length < 6) throw new Error('Must pass full name, email, location, type, password, and phone number')
+    if (args.length < 3) throw new Error('Must pass name, email, and password');
     // column names in database
-    const columns = ['name', 'email', 'location', 'type', 'password', 'number'];
-    // SQL command to insert a new row into the "Users" table
-    const text = `INSERT INTO "Users" (${columns.join(',')}) VALUES('${args.join("','")}') RETURNING *`;
+    const columns = ['name', 'email', 'password'];
     // invoke SQL command
-    return await db.query(text);
+    return await db.query(`INSERT INTO "Users" (${columns.join(',')}) VALUES('${args.join("','")}') RETURNING *`);
   },
 
-  getUser: async (email) => {
-    if (typeof email !== 'string') throw new Error('Email must be string');
+  getUser: async (email, password) => {
+    // need to ENCRYPT and check password here
+    if (typeof email !== 'string' && typeof password !== 'string') throw new Error('Invalid argument types');
     return await db.query(`SELECT * FROM "Users" WHERE email='${email}'`);
   },
 
-  insertEvent: async (...args) => {
-    // eight parameters must be passed
-    if (args.length < 8) throw new Error('Must pass host, title, service, location, date, fee, description, status');
+  changeName: async (email, name) => {
+    if (typeof name !== 'string' && typeof email !== 'string') throw new Error('Invalid argument types');
+    return await db.query(`UPDATE "Users" SET name='${name}' WHERE email='${email}' RETURNING *`);
+  },
+
+  changePassword: async (email, origPass, newPass) => {
+    // must check old password and ENCRYPT new password
+    if (typeof origPass !== 'string' && typeof email !== 'string') throw new Error('Invalid argument types');
+    return await db.query(`UPDATE "Users" SET password='${newPass}' WHERE email='${email}' AND password='${origPass}'`);
+  },
+
+
+
+
+
+  // =====>Threads<===== \\
+  addThread: async (...args) => {
+    // two parameters must be passed
+    if (args.length < 2) throw new Error('Must pass creator and password');
     // column names in database
-    const columns = ['host', 'title', 'service', 'location', 'date', 'fee', 'description', 'status'];
+    const columns = ['creator', 'password'];
     // invoke SQL command
-    return await db.query(`INSERT INTO "Events" (${columns.join(',')}) VALUES('${args.join("','")}') RETURNING *`);
+    return await db.query(`INSERT INTO "Threads" (${columns.join(',')}) VALUES((SELECT id FROM "Users" WHERE email='${args[0]}'), '${args[1]}') RETURNING *`);
   },
 
-  getEvent: async (eventId) => {
-      // eventId must be number
-      if (typeof eventId !== 'number') throw new Error('Must pass number');
-      return await db.query(`SELECT * FROM "Events" WHERE "Id"=${eventId}`);
+  getThread: async (threadId, password) => {
+    // needs ENCRYPTing
+    if (typeof threadId !== 'number' && typeof password !== 'string') throw new Error('Invalid argument types');
+    return await db.query(`SELECT * FROM "Threads" WHERE id='${threadId}' AND password='${password}'`);
   },
 
-  cancelEvent: async (eventId) => {
-      // eventId must be number
-      if (typeof eventId !== 'number') throw new Error('Must pass a number');
-      return await db.query(`UPDATE "Events" SET status='cancelled' WHERE "Id"=${eventId}`);
+  deleteThread: async (threadId, password) => {
+    if (typeof threadId !== 'number' && typeof password !== 'string') throw new Error('Invalid argument types');
+    // return await db.query(`UPDATE "Events" SET status='cancelled' WHERE "Id"=${eventId}`);
+    // delete all memberships
+    // delete all messages
+    return await db.query(`DELETE FROM "Threads" WHERE id='${threadId}'`);
   },
 
-  insertRSVPS: async (...args) => {
-      // two parameters must be passed
-      if (args.length < 2) throw new Error('Must pass event id and user id');
-      // column names in database
-      const columns = ['event', 'attendee'];
-      // invoke SQL command
-      return await db.query(`INSERT INTO "RSVPS" (${columns.join(',')}) VALUES('${args.join("','")}') RETURNING *`);
+
+
+
+
+  // =====>Memberships<===== \\
+  joinThread: async (threadId, threadPassword, userId) => {
+    // column names in database
+    const columns = ['thread', 'member'];
+    // invoke SQL command
+    // NEED TO ADD TITLE TO CHATS and PASSword checking?
+    return await db.query(`INSERT INTO "Memberships" (${columns.join(',')}) 
+      VALUES((SELECT id FROM "Threads" WHERE password='${threadPassword}'), '${userId}') RETURNING *`);
   },
 
-  getUsersRSVPList: async (userId) => {
-    if (typeof userId !== 'number') throw new Error('Must pass a number');
-    return await db.query(`SELECT * FROM "RSVPS" WHERE attendee='${userId}'`);
+  getThreadMembers: async (threadId) => {
+    if (typeof threadId !== 'number') throw new Error('Invalid argument types');
+    return await db.query(`SELECT name FROM "Memberships" 
+      LEFT JOIN "Users" ON "Memberships".member="Users".id
+      WHERE thread='${threadId}'`);
   },
 
-  getEventsRSVPList: async (eventId) => {
-    if (typeof eventId !== 'number') throw new Error('Must pass a number');
-    return await db.query(`SELECT name, email, number FROM "RSVPS" LEFT JOIN "Users" ON "Users"."Id"="RSVPS".attendee WHERE event='${eventId}'`);
+  leaveThread: async (threadId, userId) => {
+    if (typeof threadId !== 'number' && typeof userId !== 'number') throw new Error('Invalid argument types');
+    // delete one person from a thread
+    return await db.query(`DELETE FROM "Memberships" WHERE thread='${threadId}' AND member='${userId}'`);
   },
 
-  getHostedEvents: async (hostId) => {
-    if (typeof hostId !== 'number') throw new Error('Must pass a number');
-    return await db.query(`SELECT * FROM "Events" WHERE host='${hostId}'`);
+  deleteAllThreadMemberships: async (threadId) => {
+    if (typeof threadId !== 'number') throw new Error('Invalid argument types');
+    // delete all memberships to one thread
+    return await db.query(`DELETE FROM "Memberships" WHERE thread='${threadId}'`);
   },
 
-  getEventByService: async (service) => {
-    if (typeof service !== 'string') throw new Error('Must pass a string');
-    return await db.query(`SELECT * FROM "Events" WHERE service='${service}'`);
+
+
+
+
+  // =====>Messages<===== \\
+  writeMessage: async (...args) => {
+    // six parameters must be passed
+    if (args.length < 4) throw new Error('Must pass author, thread, time, and text');
+    // column names in database
+    const columns = ['author', 'thread', 'time', 'text'];
+    // invoke SQL command
+    return await db.query(`INSERT INTO "Messages" (${columns.join(',')}) VALUES('${args.join("','")}') RETURNING *`);
   },
 
-  getEventByLocation: async (location) => {
-    if (typeof location !== 'string') throw new Error('Must pass a string');
-    return await db.query(`SELECT * FROM "Events" WHERE location='${location}'`);
+  deleteAllThreadMessages: async (threadId) => {
+    if (typeof threadId !== 'number') throw new Error('Invalid argument types');
+    // delete all messages in one thread
+  },
+
+  getAThreadsMessages: async (threadId, password) => {
+    if (typeof threadId !== 'number' && typeof password !== 'number') throw new Error('Invalid argument types');
+    // check password and get messages
   }
+
+  // getEventsRSVPList: async (eventId) => {
+  //   if (typeof eventId !== 'number') throw new Error('Must pass a number');
+  //   return await db.query(`SELECT name, email, number FROM "RSVPS" LEFT JOIN "Users" ON "Users"."Id"="RSVPS".attendee WHERE event='${eventId}'`);
+  // },
 }
--- src/server/server.js
+++ src/server/server.js
@@ -2,8 +2,25 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const db = require('./db/db.js');
-const userController = require('./controllers/userController.js');
-const eventsController = require('./controllers/userController.js');
+
+// db.addUser('Hannah', 'fake@gmail.com', 'poo');
+// db.addUser('Greg', 'faker@gmail.com', 'poo');
+// db.addUser('Tsion', 'fakey@gmail.com', 'poo');
+// db.addUser('Seano', 'fakest@gmail.com', 'poo');
+
+// db.getUser('fakest@gmail.com').then(data => console.log(data));
+// db.changeName('fakest@gmail.com', 'Sean').then(data => console.log(data));
+// db.changePassword('fakest@gmail.com', 'poo', 'poop').then(data => console.log(data));
+
+// db.addThread('fakest@gmail.com', 'password');
+// db.addThread('fakest@gmail.com', 'password');
+// db.getThread(1, 'password').then(data => {console.log(data)});
+// db.deleteThread(2, 'password');
+
+// db.joinThread(2, 'password', 1).then(data => console.log(data));
+// db.joinThread(2, 'password', 2).then(data => console.log(data));
+// db.getThreadMembers(2).then(data => console.log(data));
+// db.leaveThread(2,1).then(data => {console.log(data)});
 
 app.listen(3000, (err) => {
   if (err) console.log(err);
